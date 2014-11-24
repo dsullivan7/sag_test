1a5958851ec2c61876b045dc9dce63b9bdfe98ad
dsullivan7 <dbsullivan23@gmail.com>
2014-11-24 15:00:06 +0100

    first commit

---
+++ bench_rcv1_sag.py
@@ -0,0 +1,69 @@
+import numpy as np
+from scipy import io
+
+import matplotlib.pyplot as plt
+import math
+
+from sklearn.linear_model import (SGDClassifier, SAGClassifier,
+                                  SGDRegressor, SAGRegressor)
+
+data = io.loadmat('rcv1_train.binary.mat')
+# data = io.loadmat('covtype.libsvm.binary.mat')
+
+X, y = data['X'], data['y'].ravel()
+
+# shuffle to balance the data
+# rng = np.random.RandomState(42)
+# order = np.argsort(rng.randn(len(X)))
+# X, y = X[order], y[order]
+
+# subsample so it's fast
+# X, y = X[:20000].copy(), y[:20000].copy()
+
+# cast for sklearn
+X = X.astype(np.float64)
+y = y.astype(np.int)
+
+# make it -1 and 1 for tracking log loss
+# y[y == 1] = -1
+# y[y == 2] = 1
+
+# Split data
+n_samples, n_features = X.shape
+X_train, y_train, X_test, y_test = \
+    X[:n_samples // 2], y[:n_samples // 2], \
+    X[n_samples // 2:], y[n_samples // 2:]
+
+alpha = .00001
+pobj = []
+
+n_iter_range = list(range(1, 100, 5))
+clfs = [
+    ("SGDClassifier", SGDClassifier, [], []),
+    ("SAGClassifier", SAGClassifier, [], []),
+    ]
+plt.close('all')
+
+for name, clf_constructor, pobj, score in clfs:
+    for n_iter in n_iter_range:
+        clf = clf_constructor(eta0=.01, alpha=alpha,
+                              n_iter=n_iter, random_state=42)
+        clf.loss = "log"
+        clf.learning_rate = "constant"
+        clf.fit(X_train, y_train)
+        w = clf.coef_.ravel()
+        this_pobj = np.mean(np.log(1. + np.exp(-y_train * (X_train.dot(w) +
+                                                           clf.intercept_))))
+        this_pobj += alpha * np.dot(w, w) / 2.
+        pobj.append(math.log(this_pobj))
+        score.append(clf.score(X_test, y_test))
+
+        print(name + " %1.6f %f" % (clf.score(X_test, y_test), this_pobj))
+
+    print("")
+    plt.plot(n_iter_range, pobj, label=name)
+
+plt.legend(loc="upper right")
+plt.xlabel("n_iter")
+plt.ylabel("pobj")
+plt.show()
