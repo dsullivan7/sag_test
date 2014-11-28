d86ce0c2d076d9f3f769daaa4cb195c54293d411
dsullivan7 <dbsullivan23@gmail.com>
2014-11-28 13:26:24 +0100

    changing to tol benchmark

--- bench_rcv1_sag.py
+++ bench_rcv1_sag.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import math
 import time
 import pure_sag
+from sklearn import cross_validation
 from sklearn.utils import shuffle
 from sklearn.base import clone
 from sklearn.linear_model import (SGDClassifier, SAGClassifier,
@@ -17,7 +18,7 @@ X, y = data['X'], data['y'].ravel()
 X, y = shuffle(X, y, random_state=42)
 
 # subsample so it's fast
-X, y = X[:100000].copy(), y[:100000].copy()
+X, y = X[:1000].copy(), y[:1000].copy()
 
 # cast for sklearn
 X = X.astype(np.float64)
@@ -31,10 +32,10 @@ y[y == 2] = 1
 n_samples, n_features = X.shape
 training_percent = .7
 training_num = int(training_percent * n_samples)
-X_train, y_train, X_test, y_test = \
-    X[:training_num], y[:training_num], \
-    X[training_num:], y[training_num:]
-# X_train, y_train, X_test, y_test = X, y, X, y
+# X_train, y_train, X_test, y_test = \
+#     X[:training_num], y[:training_num], \
+#     X[training_num:], y[training_num:]
+X_train, y_train, X_test, y_test = X, y, X, y
 
 # alpha = .0000001
 # eta = 4.0
@@ -43,17 +44,18 @@ alpha = .01
 eta = .00000004
 pobj = []
 
-# n_iter_range = list(range(1, 50, 5))
-n_iter_range = list(range(1, 100, 5))
-# n_iter_range = list(range(1, 500, 50))
+# n_iter_range = list(range(1, 100, 5))
+tol_range = [.01, .001, .0001, .00001]
+log_tols = np.log10(tol_range)
 
 
 clfs = [
-    ("SGDClassifier", SGDClassifier(eta0=eta, alpha=alpha, loss='log',
-     learning_rate='constant'), [], [], [0]),
-    ("ASGDClassifier", SGDClassifier(eta0=eta, alpha=alpha, loss='log',
-     learning_rate='constant', average=True), [], [], [0]),
-    ("SAGClassifier", SAGClassifier(eta0='auto', alpha=alpha, random_state=42), [], [], [0]),
+    # ("SGDClassifier", SGDClassifier(eta0=eta, alpha=alpha, loss='log',
+    #  learning_rate='constant'), [], [], []),
+    # ("ASGDClassifier", SGDClassifier(eta0=eta, alpha=alpha, loss='log',
+    #  learning_rate='constant', average=True), [], [], []),
+    ("SAGClassifier", SAGClassifier(eta0='auto', alpha=alpha, random_state=42,
+                                    max_iter=100000, verbose=True), [], [], []),
     ]
 plt.close('all')
 
@@ -64,36 +66,32 @@ def get_pobj(clf):
     p += alpha * np.dot(w, w) / 2.
     return p
 
-print('computing pobj optimal')
-pobj_opt = get_pobj(SAGClassifier(eta0='auto',
-                                  alpha=alpha,
-                                  n_iter=100).fit(X_train, y_train))
-print('done !', pobj_opt)
+# print('computing pobj optimal')
+# pobj_opt = get_pobj(SAGClassifier(eta0='auto',
+#                                   alpha=alpha,
+#                                   tol=.0000099,
+#                                   max_iter=100000,
+#                                   verbose=True).fit(X_train, y_train))
+# print('done !', pobj_opt)
 # pobj_opt = 0.0
 
 for name, clf, pobj, score, seconds in clfs:
-    for i, n_iter in enumerate(n_iter_range):
+    for i, tol in enumerate(tol_range):
+        print("tol:", tol)
         clf = clone(clf)
-        clf.set_params(n_iter=n_iter, random_state=42)
-        t1 = time.time()
-        clf.fit(X_train, y_train)
-        t2 = time.time()
-        seconds.append(seconds[i] + t2 - t1)
-
-        this_pobj = get_pobj(clf)
-
-        pobj.append(math.log10(this_pobj - pobj_opt))
-        score.append(clf.score(X_test, y_test))
-
-        print(name + " %1.6f %1.16f" % (clf.score(X_test, y_test), this_pobj))
+        clf.set_params(tol=tol, random_state=42)
+        scores = cross_validation.cross_val_score(clf, X, y, cv=4)
+        print("scores:", scores)
+        print("std:", scores.std())
+        print("")
+        print("")
+        score.append(scores.mean() + scores.std())
 
     print("")
-    # plt.plot(n_iter_range, pobj, label=name)
-    plt.plot(seconds[1:], pobj, label=name)
-    # plt.plot(seconds[1:], score, label=name)
-    # plt.plot(n_iter_range, score, label=name)
-
-plt.legend(loc="upper right")
-plt.xlabel("time (seconds)")
-plt.ylabel("log10(pobj - pogj_opt)")
+for name, clf, pobj, score, seconds in clfs:
+    plt.plot(log_tols, score, label=name)
+    plt.legend(loc="lower right")
+    plt.xlabel("log10(tol)")
+    plt.ylabel("mean cv 4 score + std")
 plt.show()
+plt.close('all')
